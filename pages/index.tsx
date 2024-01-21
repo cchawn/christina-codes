d962dec40ca4e0bc4e6d5e8dfd36840d92d297e6
Christina Chan <6462356+cchawn@users.noreply.github.com>
2024-01-21 15:54:57 -0500

    feat: remove hero post

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import Container from '../components/container';
 import MoreStories from '../components/more-stories';
-import HeroPost from '../components/hero-post';
 import Intro from '../components/intro';
 import Layout from '../components/layout';
 import { getAllPosts } from '../lib/api';
@@ -13,8 +12,6 @@ type Props = {
 };
 
 export default function Index({ allPosts }: Props) {
-  const heroPost = allPosts[0];
-  const morePosts = allPosts.slice(1);
   return (
     <>
       <Layout>
@@ -23,17 +20,7 @@ export default function Index({ allPosts }: Props) {
         </Head>
         <Container>
           <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+          {allPosts.length > 0 && <MoreStories posts={allPosts} />}
         </Container>
       </Layout>
     </>
